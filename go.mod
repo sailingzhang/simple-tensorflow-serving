c486318ecfc34021736f25b8d08c808f459499c8
sailingzhang <aa582885848@outlook.com>
2019-11-18 23:10:25 +0800

    add test_tesor.go

--- go.mod
+++ go.mod
@@ -9,6 +9,9 @@ go 1.13
 
 require (
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
+	github.com/galeone/tfgo v0.0.0-20191117210142-ceeabd7240f9
+	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.3.2
+	github.com/tensorflow/tensorflow v2.0.0+incompatible
 	google.golang.org/grpc v1.25.1
 )
--- tfService/tf_service.go
+++ tfService/tf_service.go
@@ -3,10 +3,14 @@ package tfService
 import(
 	"context"
 	"github.com/cihub/seelog"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	"github.com/tensorflow/tensorflow/tensorflow/go/op"
+
 	pb "simple-tensorflow-serving/predict_proto/proto/tensorflow_s"
 )
 
 type  TfService1 struct {
+	modelMap map[string]*tf.SavedModel
 }
 
 func (this * TfService1)Classify(ctx context.Context,req *pb.ClassificationRequest) (*pb.ClassificationResponse, error){
@@ -17,12 +21,25 @@ func (this * TfService1)Classify(ctx context.Context,req *pb.ClassificationReque
 func (this * TfService1)Regress(ctx context.Context, req *pb.RegressionRequest) (*pb.RegressionResponse, error){
 	// seelog.
 	// see
+	
 	seelog.Tracef("dgd")
 	seelog.Debugf("dsg")
+
 	return nil,nil
+
 }
 
 func (this * TfService1)Predict(ctx context.Context, req *pb.PredictRequest) (*pb.PredictResponse, error){
+	rsp := pb.PredictResponse{}
+	modelname := req.ModelSpec.GetName()
+	tag := req.GetModelSpec().GetSignatureName()
+	model, err := tf.LoadSavedModel(modelname, []string{tag}, nil)
+	if err != nil {
+		seelog.Errorf("open model=%v err, err=%v",)
+		return nil,nil
+	}
+	
+	defer model.Session.Close()
 	return nil,nil
 }
 
