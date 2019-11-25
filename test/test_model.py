0af2ef9ca7460bc9c2d652753b199d826b5cce71
sailingzhang <aa582885848@outlook.com>
2019-11-25 18:17:53 +0800

    some test for tensorflow serving

--- test/test_model.py
+++ test/test_model.py
@@ -51,17 +51,16 @@ class testNet2:
         self.sess = tf.Session()
         self.sess.as_default()
         with tf.variable_scope('mytest'):
-            self.x = tf.placeholder(shape=(2,3),dtype=tf.float32,name="input1")
-            self.y = tf.placeholder(shape=(2,3),dtype=tf.float32,name="input2")
+            self.x = tf.placeholder(shape=(None,2,3),dtype=tf.float32,name="input1")
+            self.y = tf.placeholder(shape=(None,2,3),dtype=tf.float32,name="input2")
             # self.test_var = tf.Variable([1,2,3])
             self.ret1 = tf.add(self.x,self.y,"myadd")
-            tmp = tf.transpose(self.x)
-            self.ret2 = tf.matmul(tmp,self.y,name="mymatmul")
+            self.ret2 = tf.square(self.y,name="square")
         
 
     def fit(self):
         init = tf.global_variables_initializer()
-        out =self.sess.run([self.ret1,self.ret2],feed_dict={self.x:[[1,1,1],[2,2,2]],self.y:[[3,3,3],[4,4,4]]})
+        out =self.sess.run([self.ret1,self.ret2],feed_dict={self.x:[[[1,1,1],[2,2,2]]],self.y:[[[3,3,3],[4,4,4]]]})
         print("same out0={},out1={}".format(out[0],out[1]))
     def build_save(self):
         builder = tf.saved_model.builder.SavedModelBuilder("golangtestmodel")
